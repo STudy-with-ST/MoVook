4b57640f796402a7cc20979d1ffbe74325269b7b
shyunju7 <shyunju7@naver.com>
2022-12-15 17:59:45 +0900

    Style Intro Page(이미지 및 텍스트 적용)

--- front/movook-app/src/pages/Intro/index.js
+++ front/movook-app/src/pages/Intro/index.js
@@ -1,7 +1,20 @@
 import React from "react";
 import * as S from "./style.js";
+import MoVookImg from "../../assets/img/intro/img_intro.png";
+import { Link } from "react-router-dom";
 const Intro = () => {
-  return <S.IntroContainer>Intro Page</S.IntroContainer>;
+  return (
+    <S.IntroContainer>
+      <S.IntroMoVookImg src={MoVookImg} alt="movook" />
+      <S.TitleWrapper>
+        <S.IntroTitle>내가 본</S.IntroTitle>
+        <S.IntroTitle>책부터 영화까지</S.IntroTitle>
+        <S.IntroTitle>한번에 기록해보세요!</S.IntroTitle>
+      </S.TitleWrapper>
+
+      <Link to="/login">MOVOOK 시작하기</Link>
+    </S.IntroContainer>
+  );
 };
 
 export default Intro;
--- front/movook-app/src/pages/Intro/style.js
+++ front/movook-app/src/pages/Intro/style.js
@@ -1,7 +1,41 @@
 import styled from "styled-components";
 
 export const IntroContainer = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   width: 100%;
   height: 100vh;
   background-color: #99c9a6;
+  border-bottom-left-radius: 500px;
+`;
+
+export const IntroMoVookImg = styled.img`
+  position: absolute;
+  top: 0px;
+  right: 0px;
+  width: 43%;
+`;
+
+export const TitleWrapper = styled.div`
+  width: 60%;
+  max-width: 980px;
+  background-color: transparent;
+  text-align: start;
+  z-index: 1;
+`;
+
+export const IntroTitle = styled.h1`
+  font-style: normal;
+  font-weight: 700;
+  font-size: 3.25rem;
+  line-height: 80px;
+  letter-spacing: 0.1em;
+  color: #fff;
+  @media screen and (max-width: 640px) {
+    font-size: 2rem;
+    line-height: 50px;
+  }
 `;
