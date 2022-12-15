a2deebce7651d65f26214e7470aebab0500e50dd
shyunju7 <shyunju7@naver.com>
2022-12-15 23:06:47 +0900

    Style color-palette 설정 및 기본 폰트 단위설정

--- front/movook-app/src/styles/GlobalStyle.js
+++ front/movook-app/src/styles/GlobalStyle.js
@@ -3,7 +3,15 @@ import reset from "styled-reset";
 
 const GlobalStyles = createGlobalStyle`
   ${reset}
-  @font-face {
+
+html {
+  --color-text: #333333;
+  --color-text-secondary: #999999;
+  --color-primary: #99C9A6;
+  font-size: 62.5%; 
+}
+
+@font-face {
      font-family: 'S-CoreDream-4Regular';
      src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_six@1.2/S-CoreDream-3Light.woff') format('woff');
      font-weight: normal;
@@ -16,40 +24,39 @@ const GlobalStyles = createGlobalStyle`
      font-style: normal;
 }
 
-  body {
-
+body {
   margin: 0;
-  font-family: "S-CoreDream-3Light", -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+  font-family: "S-CoreDream-4Regular", -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
     'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
     sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color : #333;
-  } 
+  color: var(--color-text);
+} 
 
-  a{
-        text-decoration: none;
-        color: inherit;
-    }
-    *{
-        box-sizing: border-box;
-    }
-    input, textarea { 
-      -moz-user-select: auto;
-      -webkit-user-select: auto;
-      -ms-user-select: auto;
-      user-select: auto;
-    }
-    input:focus {
-      outline: none;
-    }
+a{
+      text-decoration: none;
+      color: inherit;
+  }
+  *{
+      box-sizing: border-box;
+  }
+  input, textarea { 
+    -moz-user-select: auto;
+    -webkit-user-select: auto;
+    -ms-user-select: auto;
+    user-select: auto;
+  }
+  input:focus {
+    outline: none;
+  }
 
-    button {
-      border: none;
-      background: none;
-      padding: 0;
-      cursor: pointer;
-    }
+  button {
+    border: none;
+    background: none;
+    padding: 0;
+    cursor: pointer;
+  }
 `;
 
 export default GlobalStyles;
