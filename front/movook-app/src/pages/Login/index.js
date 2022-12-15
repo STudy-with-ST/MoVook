c75b7d4e5c9a03f0efa2fc97de93a83e7923aab0
shyunju7 <shyunju7@naver.com>
2022-12-15 23:43:38 +0900

    Feat Login input

--- front/movook-app/src/pages/Login/index.js
+++ front/movook-app/src/pages/Login/index.js
@@ -1,7 +1,41 @@
 import React from "react";
-
+import BasicInput from "../../components/common/BasicInput";
+import { useInput } from "../../hooks/useInput";
 const Login = () => {
-  return <div>Login Page</div>;
+  const [userId, onChangeUserId] = useInput("");
+  const [userPw, onChangeUserPw] = useInput("");
+  const handleOnClick = (e) => {
+    e.preventDefault();
+
+    const user = {
+      id: userId,
+      password: userPw,
+    };
+
+    console.log(user);
+  };
+  return (
+    <div>
+      <form method="POST">
+        <BasicInput
+          id="input-id"
+          text="ID"
+          placeholder="Enter Your ID"
+          type="text"
+          value={userId}
+          onChange={onChangeUserId}
+        />
+        <BasicInput
+          text="Password"
+          placeholder="Enter Your Password"
+          type="password"
+          value={userPw}
+          onChange={onChangeUserPw}
+        />
+        <input type="submit" value="로그인" onClick={handleOnClick} />
+      </form>
+    </div>
+  );
 };
 
 export default Login;
