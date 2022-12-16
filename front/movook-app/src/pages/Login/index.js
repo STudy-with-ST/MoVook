a6dd4e33f463e2899e5370fbb6cba908cd525e47
shyunju7 <shyunju7@naver.com>
2022-12-16 17:06:43 +0900

    Style Login UI

--- front/movook-app/src/pages/Login/index.js
+++ front/movook-app/src/pages/Login/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import BasicButton from "../../components/common/BasicButton";
 import BasicInput from "../../components/common/BasicInput";
 import { useInput } from "../../hooks/useInput";
+import MovookImg from "../../assets/img/common/img_movook.png";
+import * as S from "./style.js";
 const Login = () => {
   const [userId, onChangeUserId] = useInput("");
   const [userPw, onChangeUserPw] = useInput("");
@@ -15,26 +17,37 @@ const Login = () => {
     console.log("user: ", user);
   };
   return (
-    <div>
-      <form method="POST">
-        <BasicInput
-          id="input-id"
-          text="ID"
-          placeholder="Enter Your ID"
-          type="text"
-          value={userId}
-          onChange={onChangeUserId}
-        />
-        <BasicInput
-          text="Password"
-          placeholder="Enter Your Password"
-          type="password"
-          value={userPw}
-          onChange={onChangeUserPw}
-        />
-        <BasicButton text="로그인" onClick={handleOnClick} />
-      </form>
-    </div>
+    <S.LoginContainer>
+      <S.LoginFormContainer>
+        <S.PageTitle>Sign In</S.PageTitle>
+        <S.Description>
+          Don’t have a account, <S.PageLink to="/join">Sign Up</S.PageLink>
+        </S.Description>
+        <S.FormWrapper>
+          <form method="POST">
+            <BasicInput
+              id="input-id"
+              text="ID"
+              placeholder="Enter Your ID"
+              type="text"
+              value={userId}
+              onChange={onChangeUserId}
+            />
+            <BasicInput
+              text="Password"
+              placeholder="Enter Your Password"
+              type="password"
+              value={userPw}
+              onChange={onChangeUserPw}
+            />
+            <BasicButton text="로그인" onClick={handleOnClick} />
+          </form>
+        </S.FormWrapper>
+      </S.LoginFormContainer>
+      <S.ImgContainer>
+        <img src={MovookImg} width="60%" alt="movook" />
+      </S.ImgContainer>
+    </S.LoginContainer>
   );
 };
 
