a0be3dd4d2464396b0d4c005d45282a9f2a617fa
shyunju7 <shyunju7@naver.com>
2022-12-18 23:02:23 +0900

    Feat Sign Up UI & Style

--- front/movook-app/src/pages/Join/index.js
+++ front/movook-app/src/pages/Join/index.js
@@ -1,7 +1,70 @@
 import React from "react";
-
+import BasicButton from "../../components/common/BasicButton";
+import BasicInput from "../../components/common/BasicInput";
+import { useInput } from "../../hooks/useInput";
+import MovookImg from "../../assets/img/common/img_movook.png";
+import * as S from "./style.js";
 const Join = () => {
-  return <div>Join Page</div>;
+  const [userId, onChangeUserId] = useInput("");
+  const [userPw, onChangeUserPw] = useInput("");
+  const [userName, onChangeUserName] = useInput("");
+  const [userConfirmPw, onChangeUserConfirmPw] = useInput("");
+
+  const handleOnClick = (e) => {
+    e.preventDefault();
+
+    const createdUser = {
+      id: userId,
+      name: userName,
+      password: userPw,
+    };
+    console.log("createdUser: ", createdUser);
+  };
+  return (
+    <S.LoginContainer>
+      <S.LoginFormContainer>
+        <S.PageTitle>Sign Up</S.PageTitle>
+        <S.FormWrapper>
+          <form method="POST">
+            <BasicInput
+              id="input-name"
+              text="Name"
+              placeholder="Enter Your Name"
+              type="text"
+              value={userName}
+              onChange={onChangeUserName}
+            />
+            <BasicInput
+              id="input-id"
+              text="ID"
+              placeholder="Enter Your ID"
+              type="text"
+              value={userId}
+              onChange={onChangeUserId}
+            />
+            <BasicInput
+              text="Password"
+              placeholder="Enter Your Password"
+              type="password"
+              value={userPw}
+              onChange={onChangeUserPw}
+            />
+            <BasicInput
+              text="Password"
+              placeholder="Enter Your Confirm Password"
+              type="Confirm password"
+              value={userConfirmPw}
+              onChange={onChangeUserConfirmPw}
+            />
+            <BasicButton text="회원가입" onClick={handleOnClick} />
+          </form>
+        </S.FormWrapper>
+      </S.LoginFormContainer>
+      <S.ImgContainer>
+        <img src={MovookImg} width="60%" alt="movook" />
+      </S.ImgContainer>
+    </S.LoginContainer>
+  );
 };
 
 export default Join;
