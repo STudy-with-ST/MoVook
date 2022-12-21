c7839d02fe0f83e9f3d128eb21ec5f94a1395103
lee-jaewook <jaewook_lee@outlook.com>
2022-12-21 12:42:41 +0900

    jwt 로그인 구현

--- src/main/java/com/movook/service/UserService.java
+++ src/main/java/com/movook/service/UserService.java
@@ -1,5 +1,15 @@
 package com.movook.service;
 
-public interface UserService {
+import com.movook.vo.User;
 
+import java.util.Map;
+
+public interface UserService {
+    boolean join(User user) throws Exception;
+    User login(User user) throws Exception;
+    void saveRefreshToken(Map map) throws Exception;
+    Object getRefreshToken(String id) throws Exception;
+    void delRefreshToken(String id) throws Exception;
+    String searchPw(User user);
+    String changePw(User user);
 }
