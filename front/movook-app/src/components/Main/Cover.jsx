059054c943dbcf03f07b7ac0d153ac51059ec58c
안태현 <dksxogus1012@gmail.com>
2022-12-22 20:32:12 +0900

    Style: styled-component 사용으로 변경

--- front/movook-app/src/components/Main/Cover.jsx
+++ front/movook-app/src/components/Main/Cover.jsx
@@ -1,18 +1,16 @@
 import React from "react";
+import styled from "styled-components";
 
 const Cover = ({ work }) => {
-  const styleObj = {
-    height: "250px",
-    width: "170px",
-    borderRadius: work.type === "book" ? "15px" : "0px",
-    boxShadow: "0 4px 5px 1px gray",
-    margin: "10px",
-  };
-  return (
-    <div>
-      <img style={styleObj} src={work.url} alt="영화 이미지" />
-    </div>
-  );
+  return <Poster work={work} src={work.url} alt="이미지" />;
 };
 
 export default Cover;
+
+const Poster = styled.img`
+  height: 250px;
+  width: 170px;
+  border-radius: ${(props) => (props.work.type === "book" ? "15px" : "0px")};
+  box-shadow: 0 4px 5px 1px gray;
+  margin: 10px;
+`;
