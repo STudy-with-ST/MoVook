c27899da2321e7724aad0861f3a0706f658b5394
lee-jaewook <jaewook_lee@outlook.com>
2022-12-29 19:33:16 +0900

    SearchVo @ApiModelProperty 추가

--- back/src/main/java/com/movook/vo/SearchVo.java
+++ back/src/main/java/com/movook/vo/SearchVo.java
@@ -1,16 +1,26 @@
 package com.movook.vo;
 
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiParam;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-
+@AllArgsConstructor
 @Setter
 @Getter
 public class SearchVo {
+
+	@ApiModelProperty(value = "현재 페이지 번호")
 	private int page;			// 현재 페이지 번호
-	private int recordSize;		// 페이지당 출력할 데이터 개수 
+	@ApiModelProperty(value = "페이지당 출력할 데이터 개수")
+	private int recordSize;		// 페이지당 출력할 데이터 개수
+	@ApiModelProperty(value = "화면 하단에 출력할 페이지 사이즈")
 	private int pageSize;		// 화면 하단에 출력할 페이지 사이즈
+	@ApiModelProperty(value = "검색 키워드")
 	private String keyword;		// 검색 키워드
+	@ApiModelProperty(value = "검색 유형")
 	private String searchType;	// 검색 유형
 	
 	public SearchVo() {
